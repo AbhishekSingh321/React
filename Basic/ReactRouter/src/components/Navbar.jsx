667db5bf4fd7389ac3468279900e35ca69f2d5db
AbhishekSingh321 <singhabhi292004@gmail.com>
2025-12-29 00:00:56 +0530

    dashboard route and integrate navbar with app routing

--- Basic/ReactRouter/src/components/Navbar.jsx
+++ Basic/ReactRouter/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react'
 
 function Navbar() {
+  const [login,setLogin]=useState(false)
+  const navigate = useNavigate();
+
+  const handleAuth = () => {
+    if (!login) {
+      setLogin(true);
+      navigate("/dashboard");
+    } else {
+      setLogin(false);
+      navigate("/");
+    }
+  };
+
   return (
     <nav>
       <Link to="/">Home</Link>
       &nbsp; &nbsp;
       <Link to="/about">About</Link>
+      &nbsp; &nbsp;
+      <button onClick={handleAuth}>{login?'Logout':'Login'}</button>
     </nav>
   );
 }
