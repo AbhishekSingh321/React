950e23567aece1f632238f98530f7aa57e2e288f
AbhishekSingh321 <singhabhi292004@gmail.com>
2026-01-02 23:51:39 +0530

    improve auth routing flow

--- Context+Auth/src/App.jsx
+++ Context+Auth/src/App.jsx
@@ -1,13 +1,20 @@
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, Navigate} from 'react-router-dom'
 import Login from "./pages/Login"
 import Dashboard from "./pages/Dashboard"
 import PrivateRoute from "./routes/PrivateRoute"
+import {useAuth} from './hooks/useAuth'
 
 
 function App() {
+  const { isAuthenticated } = useAuth();
   return (
     <>
       <Routes>
+          <Route path="/"
+            element={
+              isAuthenticated ? (<Navigate to="/dashboard" replace />) : (<Navigate to="/login" replace />)
+            }
+          />
         <Route path='/login' element={<Login/>}/>
         <Route path='/dashboard' element={
           <PrivateRoute>
